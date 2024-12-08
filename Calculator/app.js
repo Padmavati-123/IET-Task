2fc2d8ce18a57247b46ce556e9d55b8f4e70c0e2
thanmayasrii <thanmayasrinelluri.231it043@nitk.edu.in>
2024-12-08 17:52:21 +0530

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -25,3 +25,31 @@ function computesub(){
     document.getElementById('output').innerText = result;
 
 }
+
+function computemul(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 * num2;
+    document.getElementById('output').innerText = result;
+
+}
+
+function computediv(){
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        document.getElementById('output').innerText = 'Please Enter Valid Numbers';
+        return;
+    }
+
+    const result = num1 / num2;
+    document.getElementById('output').innerText = result;
+
+}
