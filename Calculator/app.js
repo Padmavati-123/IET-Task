393cbfead59140979662d912d607886c3ceab7e5
Hanvitha-1464 <petetihanvitha.231it048@nitk.edu.in>
2024-12-09 17:57:39 +0530

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -25,7 +25,6 @@ function computesub(){
     document.getElementById('output').innerText = result;
 
 }
-
 function computemul(){
     const num1 = parseInt(document.getElementById('num1').value);
     const num2 = parseInt(document.getElementById('num2').value);
